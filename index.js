a884fec1e25bd70b3f9f2031455b07694a10f4ac
Elijah <bundielly@gmail.com>
2022-06-02 15:31:27 +0300

    new module

---
+++ index.js
@@ -0,0 +1,42 @@
+/*
+let myArray = ["a", "b", "c", "d", "e", "f", "g"]
+
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i])
+}
+*/
+/*
+let myArray = ["a", "b", "c", "d", "e", "f", "g"]
+
+let j = 0
+while (j < myArray.length) {
+    console.log(myArray[j++])
+}
+*/
+/*
+const myArray = ["a", "b", "c", "d", "e", "f", "g"]
+
+for(const letter of myArray) {
+    console.log(letter)
+}
+*/
+/*
+const address = {
+    street1: '11 Broadway',
+    street2: '2nd Floor',
+    city: 'New York',
+    state: 'NY',
+    zipCode: '10004',
+}
+
+for (key in address) {
+    console.log(key)
+}
+
+for (key in address) {
+    console.log(address[key])
+}
+*/
+
+
+
